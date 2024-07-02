23f42fef6e8f01359d2e2ec6f0ead2996838815a
Jason Foat <jfoat@salk.edu>
2024-07-02 17:19:36 +0000

    Works, but must be numpy==1.26

--- stac-mjx/main.py
+++ stac-mjx/main.py
@@ -119,6 +119,7 @@ def hydra_entry(cfg: DictConfig):
 
     # XLA flags for Nvidia GPU
     if xla_bridge.get_backend().platform == "gpu":
+        '''
         os.environ["XLA_FLAGS"] = (
             "--xla_gpu_enable_triton_softmax_fusion=true "
             "--xla_gpu_triton_gemm_any=True "
@@ -126,6 +127,7 @@ def hydra_entry(cfg: DictConfig):
             "--xla_gpu_enable_latency_hiding_scheduler=true "
             "--xla_gpu_enable_highest_priority_async_stream=true "
         )
+        '''
         # Set N_GPUS
         utils.params["N_GPUS"] = jax.local_device_count("gpu")
 
