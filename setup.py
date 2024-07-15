4c33b0054ce41e739f464f201bbf7a67f9a2066d
Jason Foat <jfoat@salk.edu>
2024-07-15 04:59:20 +0000

    Adding charles-dev version of setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import setup, find_packages
     from the environment.yaml install. However it's missing some includes
     to make viz_usage.py work.
     
-    The commented version below is included because for the sake of 
-    comparison only. 
+    The commented version below is from charles-dev, and is included because 
+    for the sake of comparison only.
 '''
 
 setup(
@@ -39,31 +39,37 @@ setup(
 '''
 
 """Setup file for stac."""
-from setuptools import setup, find_packages
-
 setup(
     name="stac-mjx",
     version="0.0.1",
+    python_requires=">=3.11",
     packages=find_packages(),
     install_requires=[
         "six >= 1.12.0",
         "clize >= 4.0.3",
         "absl-py >= 0.7.1",
-        "mujoco-mjx >= 3.0.0",
+        "mujoco-mjx >= 3.1.5",
+        "dm_control",
+        "jaxopt",
+        "flax",
         "enum34",
         "future",
-        # 'futures',
-        "glfw",
         "lxml",
-        "numpy",
+        "mediapy",
+        "numpy < 2.0",
         "pyopengl",
         "pyparsing",
         "h5py >= 2.9.0",
         "scipy >= 1.2.1",
         "pyyaml",
         "opencv-python",
-        
+        "imageio",
+        "matplotlib",
+        "hydra-core",
+        "optax",
+        "colorama",
+        "imageio[pyav]",
+        "imageio[ffmpeg]",
     ],
 )
-
 '''
