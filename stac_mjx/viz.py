592c351c022e215eda1f93192ef7c05c32d13abd
Jason Foat <jfoat@salk.edu>
2024-07-22 18:48:40 +0000

    Black viz.py

--- stac_mjx/viz.py
+++ stac_mjx/viz.py
@@ -1,7 +1,6 @@
 """A collection mujoco-mjx vizualization utilities.
 """
 
-
 from dm_control import mjcf
 from dm_control.locomotion.walkers import rescale
 from dm_control.mujoco.wrapper.mjbindings import enums
@@ -196,8 +195,7 @@ def overlay_viz(
     save_path,
     camera: Text = "close_profile",
 ):
-    """TBD
-    """
+    """TBD."""
     scene_option = wrapper.MjvOption()
     # scene_option.geomgroup[1] = 0
     scene_option.geomgroup[2] = 1
@@ -308,8 +306,7 @@ def overlay_viz(
 
 
 def mujoco_viz(data_path, model_xml, n_frames, save_path, start_frame: int = 0):
-    """Renders forward kinematics from keypoint positions.
-    """
+    """Renders forward kinematics from keypoint positions."""
     scene_option = mujoco.MjvOption()
     # scene_option.geomgroup[1] = 0
     scene_option.geomgroup[2] = 1
