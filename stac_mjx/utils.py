6495ffc1cf98208cb40d6264c1cbb736e7f52b8b
Charles Zhang <charleszhang216@mgmail.com>
2024-07-31 15:14:48 -0400

    python-api merge

--- stac_mjx/utils.py
+++ stac_mjx/utils.py
@@ -1,7 +1,5 @@
 """Utility functions to load data from .mat .yaml and .h5 files."""
 
-import numpy as np
-import h5py
 import os
 import yaml
 import scipy.io as spio
@@ -57,7 +55,7 @@ def _load_params(param_path):
     return params
 
 
-def init_params(cfg):
+def init_params(cfg: dict):
     """Initialize parameters from config."""
     global params
     params = cfg
