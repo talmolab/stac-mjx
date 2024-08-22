dbbe57909f850244893d70bfbfe83b742ee60bae
Charles Zhang <charles.zhang216@gmail.com>
2024-08-22 00:09:48 -0400

    update run_rodent so hydra_entry has no return values

--- run_rodent.py
+++ run_rodent.py
@@ -13,15 +13,28 @@ from stac_mjx import utils
 from pathlib import Path
 
 
+def load_and_run_stac(stac_cfg, model_cfg):
+    base_path = Path.cwd()
+
+    data_path = base_path / stac_cfg.data_path
+    kp_data, sorted_kp_names = utils.load_data(data_path, model_cfg)
+
+    fit_path, transform_path = main.run_stac(
+        stac_cfg, model_cfg, kp_data, sorted_kp_names, base_path=base_path
+    )
+
+    logging.info(
+        f"Run complete. \n fit path: {fit_path} \n transform path: {transform_path}"
+    )
+
+
 @hydra.main(config_path="./configs", config_name="stac", version_base=None)
 def hydra_entry(stac_cfg: DictConfig):
-    # Initialize configs and convert to dictionaries
+    # Initialize configs
     model_cfg = hydra.compose(config_name="rodent")
     logging.info(f"cfg: {OmegaConf.to_yaml(stac_cfg)}")
     logging.info(f"model_cfg: {OmegaConf.to_yaml(model_cfg)}")
     model_cfg = OmegaConf.to_container(model_cfg, resolve=True)
-
-    base_path = Path.cwd()
 
     # XLA flags for Nvidia GPU
     if xla_bridge.get_backend().platform == "gpu":
@@ -30,16 +43,8 @@ def hydra_entry(stac_cfg: DictConfig):
             "--xla_gpu_triton_gemm_any=True "
         )
 
-    data_path = base_path / stac_cfg.data_path
-    kp_data, sorted_kp_names = utils.load_data(data_path, model_cfg)
-
-    return main.run_stac(
-        stac_cfg, model_cfg, kp_data, sorted_kp_names, base_path=base_path
-    )
+    load_and_run_stac(stac_cfg, model_cfg)
 
 
 if __name__ == "__main__":
-    fit_path, transform_path = hydra_entry()
-    logging.info(
-        f"Run complete. \n fit path: {fit_path} \n transform path: {transform_path}"
-    )
+    hydra_entry()
