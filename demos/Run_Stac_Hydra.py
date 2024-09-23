3ac92a32524c4d116e200c44548ea916a6a8ce9a
elliottabe <elliottabe@gmail.com>
2024-09-23 10:38:51 -0700

    fix head/thorax site names

--- demos/Run_Stac_Hydra.py
+++ demos/Run_Stac_Hydra.py
@@ -1,5 +1,6 @@
 import os 
 
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'  # Use GPU 1
 import stac_mjx 
 from pathlib import Path
 from jax import numpy as jp
@@ -22,25 +23,25 @@ def parse_hydra_config(cfg: DictConfig):
     model_cfg = cfg.model
     base_path = Path.cwd().parent
     
-    # tredmill_data = pd.read_csv(data_dir / "wt_berlin_linear_treadmill_dataset.csv")
-    # kp_names = ['head', 'thorax', 'abdomen', 'r1', 'r2', 'r3', 'l1', 'l2', 'l3']
-    # coords = ['_x', '_y', '_z']
-    # df_names = [kp+coord for kp in kp_names for coord in coords]
-    # kp_data_all = tredmill_data[df_names].values
-    # sorted_kp_names = kp_names
-    # kp_data = model_cfg['MOCAP_SCALE_FACTOR']*kp_data_all.copy()
+    tredmill_data = pd.read_csv(stac_cfg.data_path)
+    kp_names = ['head', 'thorax', 'abdomen', 'r1', 'r2', 'r3', 'l1', 'l2', 'l3']
+    coords = ['_x', '_y', '_z']
+    df_names = [kp+coord for kp in kp_names for coord in coords]
+    kp_data_all = tredmill_data[df_names].values
+    sorted_kp_names = kp_names
+    kp_data = model_cfg['MOCAP_SCALE_FACTOR']*kp_data_all.copy()
 
-    import stac_mjx.io_dict_to_hdf5 as ioh5
-    data_path = base_path / stac_cfg.data_path
-    bout_dict = ioh5.load(data_path)
-    legs_data = ['L1', 'R1', 'L2','R2', 'L3','R3']
-    joints_data = ['A','B','C','D','E']
-    sorted_kp_names = [leg + joint for leg in legs_data for joint in joints_data]
-    xpos_all = []
-    for nbout, key in enumerate(bout_dict.keys()):
-        xpos_all.append(bout_dict[key]['inv_xpos'].reshape(bout_dict[key]['inv_xpos'].shape[0],-1))
-    kp_data = jp.concatenate(xpos_all, axis=0)
-    kp_data = kp_data * model_cfg['MOCAP_SCALE_FACTOR']
+    # import stac_mjx.io_dict_to_hdf5 as ioh5
+    # data_path = base_path / stac_cfg.data_path
+    # bout_dict = ioh5.load(data_path)
+    # legs_data = ['L1', 'R1', 'L2','R2', 'L3','R3']
+    # joints_data = ['A','B','C','D','E']
+    # sorted_kp_names = [leg + joint for leg in legs_data for joint in joints_data]
+    # xpos_all = []
+    # for nbout, key in enumerate(bout_dict.keys()):
+    #     xpos_all.append(bout_dict[key]['inv_xpos'].reshape(bout_dict[key]['inv_xpos'].shape[0],-1))
+    # kp_data = jp.concatenate(xpos_all, axis=0)
+    # kp_data = kp_data * model_cfg['MOCAP_SCALE_FACTOR']
 
     fit_path, transform_path = stac_mjx.run_stac(cfg, kp_data, sorted_kp_names, base_path=base_path)
 
