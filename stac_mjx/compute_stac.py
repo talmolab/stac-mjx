4d66cffb7cf568827d98f1077639b5fa8f66db91
elliottabe <elliottabe@gmail.com>
2024-09-23 10:38:51 -0700

    fix head/thorax site names

--- stac_mjx/compute_stac.py
+++ stac_mjx/compute_stac.py
@@ -50,9 +50,9 @@ def root_optimization(
     # necessarily exactly so. The value of 3*18 is chosen for the
     # rodent.xml, corresponding to the index of 'SpineL' keypoint.
     # For the mouse model this should be 3*5, corresponding 'Trunk'
-    root_kp_idx = 3 * 18
-    # q0.at[:3].set(kp_data[frame, :][root_kp_idx : root_kp_idx + 3])
-    q0.at[:3].set(jp.zeros(3))
+    root_kp_idx = 0
+    q0.at[:3].set(kp_data[frame, :][root_kp_idx : root_kp_idx + 3])
+    # q0.at[:3].set(jp.zeros(3))
     qs_to_opt = jp.zeros_like(q0, dtype=bool)
     qs_to_opt = qs_to_opt.at[:7].set(True)
     kps_to_opt = jp.repeat(trunk_kps, 3)
