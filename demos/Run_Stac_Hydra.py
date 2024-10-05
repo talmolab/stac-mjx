21407fa3702cf7eb1e8393203d7d8314b0ff7e40
Jason Foat <jasonfoat@gmail.com>
2024-10-05 23:55:18 +0000

    Clean up.

--- demos/Run_Stac_Hydra.py
+++ demos/Run_Stac_Hydra.py
@@ -1,35 +1,37 @@
-import os 
+import os
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'  # Use GPU 1
-import stac_mjx 
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # Use GPU 1
+import stac_mjx
 from pathlib import Path
 from jax import numpy as jp
 import mediapy as media
-import pandas as pd 
+import pandas as pd
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
-OmegaConf.register_new_resolver('eq', lambda x, y: x.lower()==y.lower())
-OmegaConf.register_new_resolver('contains', lambda x, y: x.lower() in y.lower())
-OmegaConf.register_new_resolver('resolve_default', lambda default, arg: default if arg=='' else arg)
+OmegaConf.register_new_resolver("eq", lambda x, y: x.lower() == y.lower())
+OmegaConf.register_new_resolver("contains", lambda x, y: x.lower() in y.lower())
+OmegaConf.register_new_resolver(
+    "resolve_default", lambda default, arg: default if arg == "" else arg
+)
 
 
 @hydra.main(version_base=None, config_path="../configs", config_name="config")
 def parse_hydra_config(cfg: DictConfig):
 
-    os.environ['CUDA_VISIBLE_DEVICES'] = cfg.stac.gpu  # Use GPU 1
-    # Choose parent directory as base path to make relative pathing easier 
+    os.environ["CUDA_VISIBLE_DEVICES"] = cfg.stac.gpu  # Use GPU 1
+    # Choose parent directory as base path to make relative pathing easier
     stac_cfg = cfg.stac
     model_cfg = cfg.model
     base_path = Path.cwd().parent
-    
+
     tredmill_data = pd.read_csv(stac_cfg.data_path)
-    kp_names = ['head', 'thorax', 'abdomen', 'r1', 'r2', 'r3', 'l1', 'l2', 'l3']
-    coords = ['_x', '_y', '_z']
-    df_names = [kp+coord for kp in kp_names for coord in coords]
+    kp_names = ["head", "thorax", "abdomen", "r1", "r2", "r3", "l1", "l2", "l3"]
+    coords = ["_x", "_y", "_z"]
+    df_names = [kp + coord for kp in kp_names for coord in coords]
     kp_data_all = tredmill_data[df_names].values
     sorted_kp_names = kp_names
-    kp_data = model_cfg['MOCAP_SCALE_FACTOR']*kp_data_all.copy()
+    kp_data = model_cfg["MOCAP_SCALE_FACTOR"] * kp_data_all.copy()
 
     # import stac_mjx.io_dict_to_hdf5 as ioh5
     # data_path = base_path / stac_cfg.data_path
@@ -43,22 +45,31 @@ def parse_hydra_config(cfg: DictConfig):
     # kp_data = jp.concatenate(xpos_all, axis=0)
     # kp_data = kp_data * model_cfg['MOCAP_SCALE_FACTOR']
 
-    fit_path, transform_path = stac_mjx.run_stac(cfg, kp_data, sorted_kp_names, base_path=base_path)
+    fit_path, transform_path = stac_mjx.run_stac(
+        cfg, kp_data, sorted_kp_names, base_path=base_path
+    )
 
     # set args
-    data_path = base_path / cfg.stac['transform_path']
+    data_path = base_path / cfg.stac["transform_path"]
     n_frames = 601
     save_path = base_path / "videos/direct_render_tether.mp4"
 
     # Call mujoco_viz
-    frames = stac_mjx.viz_stac(data_path, cfg, n_frames, save_path, start_frame=0, camera=1, base_path=Path.cwd().parent)
+    frames = stac_mjx.viz_stac(
+        data_path,
+        cfg,
+        n_frames,
+        save_path,
+        start_frame=0,
+        camera=1,
+        base_path=Path.cwd().parent,
+    )
 
     # Show the video in the notebook (it is also saved to the save_path)
-    media.show_video(frames, fps=model_cfg["RENDER_FPS"]) 
+    media.show_video(frames, fps=model_cfg["RENDER_FPS"])
 
-    print('Done!')
+    print("Done!")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parse_hydra_config()
