6a4a0bfee03cdf289042a589e52e4db2b7623b8b
joshjppark <joshjppark@gmail.com>
2025-01-30 19:17:18 +0000

    added more files for push; see previous commit

--- stac_mjx/__init__.py
+++ stac_mjx/__init__.py
@@ -4,3 +4,5 @@ from stac_mjx.utils import enable_xla_flags
 from stac_mjx.io import load_mocap
 from stac_mjx.main import load_configs, run_stac
 from stac_mjx.viz import viz_stac
+
+# from stac_mjx.io import load_data
