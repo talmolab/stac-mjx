61ea7e5275a37c082f85c32da4f9aceada3d2e23
joshjppark <joshjppark@gmail.com>
2025-03-06 21:53:27 +0000

    ran improved stac_core tests

--- tests/test_stac_core.py
+++ tests/test_stac_core.py
@@ -6,15 +6,14 @@ from pathlib import Path
 import mujoco
 from stac_mjx.utils import mjx_load
 import stac_mjx
+import optax
+import jaxopt
 # import jax
 
 
-TEST_DIR = Path(__file__).parent
-PROJECT_ROOT = TEST_DIR.parent.parent
+# TEST_DIR = Path(__file__).parent
+# PROJECT_ROOT = TEST_DIR.parent.parent
 
-@pytest.fixture
-def arm_model():
-    return str(PROJECT_ROOT / "stac-mjx" / "models" / "arm_model_v3_torque.xml")
 
 @pytest.fixture
 def base_path():
@@ -25,24 +24,32 @@ def config(base_path):
     return stac_mjx.load_configs(base_path / "configs")
 
 
-def test_stac_obj(arm_model, base_path, config):
+def test_stac_core_obj(config):
 
-    # get cfg, kp_data, and sorted_kp_names:
-    kp_data, sorted_kp_names = stac_mjx.load_mocap(config, base_path)
-    stac = stac_mjx.stac.Stac(arm_model, config, sorted_kp_names)
+    stac_core_obj = stac_mjx.stac_core.StacCore(1e-10)
 
-    assert sorted_kp_names == ['Shoulder', 'Elbow', 'Wrist']
-    assert isinstance(stac._root, dm_control.mjcf.element.RootElement)
-    assert isinstance(stac._mj_model, mujoco._structs.MjModel)
+    # ensure that object type is what it is
+    assert isinstance(stac_core_obj, stac_mjx.stac_core.StacCore)
+
+    # assert the object instance variables
+    assert isinstance(stac_core_obj.opt, optax._src.base.GradientTransformationExtraArgs)
+    assert isinstance(stac_core_obj.q_solver, jaxopt._src.projected_gradient.ProjectedGradient)
+    assert isinstance(stac_core_obj.m_solver, jaxopt._src.optax_wrapper.OptaxSolver)
+
+    # assert the tolerance values are correct
+    staccore1 = stac_mjx.stac_core.StacCore(tol=config.model.FTOL)
+    assert staccore1.q_solver.tol == config.model.FTOL
+    assert stac_core_obj.q_solver.tol == 1e-10
+
 
 def test_stac_core_compilations(base_path, config):
     stac_mjx.enable_xla_flags()
 
-    assert stac_mjx.stac_core._m_opt._cache_size() == 0
-    assert stac_mjx.stac_core._q_opt._cache_size() == 0
-    assert stac_mjx.stac_core.m_loss._cache_size() == 0
-
     kp_data, sorted_kp_names = stac_mjx.load_mocap(config, base_path)
+    
+    assert stac_mjx.stac_core.m_opt._cache_size() == 0
+    assert stac_mjx.stac_core.q_opt._cache_size() == 0
+
     _, _ = stac_mjx.run_stac(
         config,
         kp_data, 
@@ -50,6 +57,6 @@ def test_stac_core_compilations(base_path, config):
         base_path=base_path
         )
 
-    assert stac_mjx.stac_core._m_opt._cache_size() == 2
-    assert stac_mjx.stac_core._q_opt._cache_size() == 2
     assert stac_mjx.stac_core.m_loss._cache_size() == 0
+    assert stac_mjx.stac_core.q_opt._cache_size() == 2
+    assert stac_mjx.stac_core.m_opt._cache_size() == 2
